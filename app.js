2d6b17cea65945a8174900481949540a3d09e508
sotrnguy92 <sonnguyen092@gmail.com>
2020-10-18 14:32:14 -0700

    Created the classes and constructor functions for all employees

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-
 const render = require("./lib/htmlRenderer");
+
+const employees = [];
+
 const questions = [
     {
         type: 'input',
@@ -98,6 +99,7 @@ const internQuestions = [
 
 inquirer.prompt(questions)
     .then((answers)=>{
+
         let addEngineer = answers.addEngineer;
         if (addEngineer === "yes"){
             buildEngineer();
